3f052371604bfc0bc4e3740b48a81b772f40449a
lollipop <vladislav.krets@gmail.com>
2020-06-25 14:55:05 +0300

    django admin changes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.forms import ModelForm, ChoiceField
+
+from core.models import Topic, Item
+
+
+class TopicForm(ModelForm):
+    topic_choices = list(Topic.objects.all().values_list('id', 'name'))
+
+    def __init__(self, *args, **kwargs):
+       super(TopicForm, self).__init__(*args, **kwargs)
+       if self.instance.id:
+           CHOICES_INCLUDING_DB_VALUE = [(self.instance.id,)*2] + self.topic_choices
+           self.initial['parent_id'] = None
+           self.fields['parent_id'] = ChoiceField(
+                choices=CHOICES_INCLUDING_DB_VALUE, label='Parent name')
+       else:
+           self.fields['parent_id'] = ChoiceField(
+               choices=self.topic_choices, label='Parent name')
+
+class TopicAdmin(admin.ModelAdmin):
+    form = TopicForm
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Item)
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -2,17 +2,28 @@ from django.db import models
 
 
 class Topic(models.Model):
-    str_id = models.CharField(max_length=500)
-    name = models.CharField(max_length=500)
-    parent_id = models.IntegerField(null=True)
+    str_id = models.CharField(max_length=500, verbose_name='1c id')
+    name = models.CharField(max_length=500, verbose_name='Name')
+    parent_id = models.IntegerField(null=True, verbose_name='Parent topic')
+
+    def __str__(self):
+        return 'Topic: ' + self.name
 
 
 class Item(models.Model):
-    str_id = models.CharField(max_length=500)
-    name = models.CharField(max_length=500)
-    price = models.FloatField(null=True)
+    str_id = models.CharField(max_length=500, verbose_name='1c id')
+    name = models.CharField(max_length=500, verbose_name='Name')
+    price = models.FloatField(null=True, blank=True, verbose_name='Price')
     topic = models.ForeignKey(to=Topic,
                               on_delete=models.deletion.CASCADE,
-                              related_name='items')
-    photo = models.ImageField(upload_to='images', null=True)
+                              related_name='items', verbose_name='Topic')
+    photo = models.ImageField(upload_to='images', null=True, verbose_name='Photo')
+
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.price = None
+        super(Item, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return 'Item: ' + self.name
 
