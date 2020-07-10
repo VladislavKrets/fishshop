c78b40db240d4c95d684e3db2a082424cd8ba0c0
lollipop <vladislav.krets@gmail.com>
2020-07-11 00:03:52 +0300

    added local_settings_example

---
+++ fishshop/local_settings_example.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fishshop',
+        'USER': 'fishshop',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
+
+ALLOWED_HOSTS = '*'
